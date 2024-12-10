be21997024a8665d32da6bea7bfe082645b5e7b9
mutzon <martinutzon@gmail.com>
2024-12-10 16:43:50 +0100

    færdig med layout40

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context"
+import {s} from "./App.style"
 
 export default function App() {
-  return ( <SafeAreaProvider>
-    <SafeAreaView>
-      <Text>Lest go</Text>
+  return ( 
+  
+  <>
+  <SafeAreaProvider>
+    <SafeAreaView style = {s.app}>
+      <View style = {s.header}><Text>Header</Text></View>
+      <View style = {s.body}><Text>Body</Text></View>
+   
     </SafeAreaView>
   </SafeAreaProvider>
+     <View style = {s.footer}><Text>Footer</Text></View>
+     </>
   )
 
 
--- App.style.js
+++ App.style.js
@@ -1,7 +1,15 @@
 import { StyleSheet} from "react-native";
 
 export const s = StyleSheet.create({
+    app:{
+        flex: 1,
+        backgroundColor: "#f9f9f9"
+
+    },
+    header: {flex: 1},
+    body: {flex: 5},
+    footer:{height: 70}
 
 
-    
+
 })
